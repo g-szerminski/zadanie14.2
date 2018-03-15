8eeb49e6890aa3a745ec9ed74c441d85a9262246
Grzegorz Szermiński <szerman@poczta.fm>
2018-03-15 11:31:59 +0100

    Add exercise content - project not finished yet

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 var movies = [
 	{
+		id: 1,
 		title: 'Harry Ploter',
-		decs: 'film o drukarce'
+		decs: 'film o drukarce',
+		photo: 'https://thumb9.shutterstock.com/display_pic_with_logo/2704957/612136502/stock-photo-large-printer-format-inkjet-working-612136502.jpg'
 	},
 	{
+		id: 2,
 		title: 'Król kur',
-		decs: 'film o kurniku'
+		decs: 'film o kurniku',
+		photo: 'https://cdn.stocksnap.io/img-thumbs/280h/1HSL6PSND8.jpg'
 	}
 ];
 
 var moviesElements = movies.map(function(movie){
-	return React.createElement('li', {},
+	return React.createElement('li', {key: movie.id},
 		React.createElement('h2', {}, movie.title),
-		React.createElement('p', {}, movie.decs)
+		React.createElement('p', {}, movie.decs),
+		React.createElement('img', {src: movie.photo})
 	);
 });
 
